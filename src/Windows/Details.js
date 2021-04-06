fd1d2c210932f9b431d8fb0e81bd95d04d0f2f1e
Valeryia <valeryia.pilchuk@gmail.com>
2021-04-05 21:41:52 -0400

    linking added

--- src/Windows/Details.js
+++ src/Windows/Details.js
@@ -1,14 +1,27 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import { BrowserRouter as Router, Link, Route} from 'react-router-dom'
 
 class Details extends React.Component{
 
     render(){
         return(
             <div>
-                Course Name:
-                Professor: 
-                
+                Course Name: Introduction to Computer Science
+                <br/>
+                Professor: Willaim Skeith
+                <br/>
+                Meeting Date and Time: Mon/Wed 11:00 - 12:30
+                <br/>
+                Prerequisits: None
+                <br/>
+                Meeting Date and Time: Mon/Wed 11:00 - 12:30
+                <br/>
+                Related Courses: idk
+                <br/>
+                Meeting Date and Time: Mon/Wed 11:00 - 12:30
+                <br/>
+                Link to Syllabus Page
             </div>
         );
 
